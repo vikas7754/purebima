ef948a55f1caac3dd14c2e1c47d6460f15d03429
Vikas <vikask4590@gmail.com>
2024-07-02 23:25:20 +0530

    fixed

--- web/src/components/pages/Home/About/index.js
+++ web/src/components/pages/Home/About/index.js
@@ -8,7 +8,7 @@ function About() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/page/about-section`
+          `${process.env.NEXT_PUBLIC_API_URL}/page/contact-section`
         );
         const data = await response.json();
         setContent(data?.data?.content);
